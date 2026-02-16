dafe1d661a76fbbf63015b6dd7a2adfa0f9cb0a2
shoo224 <ranjanshushant5@gmail.com>
2026-02-16 14:48:00 +0530

    api responses set

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ dotenv.config({
 
 
 connectdb()
-
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running on the port :${process.env.PORT}`);
+    })
+})
+.catch((error) =>{
+    console.log("Error",err);
+    throw error;
+})
 
 
