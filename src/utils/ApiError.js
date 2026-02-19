a4c7948f356f2a25e2f38fb21d19f6bcb799c228
shoo224 <ranjanshushant5@gmail.com>
2026-02-19 23:38:05 +0530

    error stack fixed

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -8,6 +8,15 @@ class ApiError extends Error {
         super(message)
         this.statusCode = statusCode
         this.data=null
+        this.message=message
+        this.success=false
+        this.errors = this.error
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this,this.constructor)
+        }
     }
 }
 
